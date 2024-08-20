c839e784b6272443eaddc435fbcf8e2ec447592f
JoshgunK <joshgunvk@code.edu.az>
2024-08-20 21:24:10 +0400

    done

--- ProniaMVC/obj/Debug/net8.0/ProniaMVC.AssemblyInfo.cs
+++ ProniaMVC/obj/Debug/net8.0/ProniaMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProniaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3cd0fd882304317ba5301b6cf67ef255eba8684")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec55510b86345e24923a0913d0184c9a8cf6bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProniaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProniaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
